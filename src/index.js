8eb231101376d6ad830426b41b0450985f875471
Merzlikin Nikita <nikmerz2002@mail.ru>
2025-07-20 17:09:28 +0600

    feat: add a solution for the task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
-module.exports = function reverse(/* n */) {
-  throw new Error('Not implemented');
+module.exports = function reverse(n) {
+  const result = n.toString().split('').reverse().join('');
+  return parseInt(result, 10);
 };
